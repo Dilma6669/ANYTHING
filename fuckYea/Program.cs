f75802c44180b9474579a676795d87dd8f2cbc9f
dilma6669 <dpringle274@gmail.com>
2019-05-08 19:18:23 -0700

    ok first commit

---
+++ fuckYea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuckYea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.("this is a program");
+        }
+    }
+}
